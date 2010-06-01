ca0fbd1ad548f55885a3e1786f05fc65537ff5fd
ja7mad <woon10@hotmail.com>
2010-06-01 18:33:46 +0300

    1st branch commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication1
     class Program
     {
         // my first comment
+        // my first change under a branch
         static void Main(string[] args)
         {
         }
